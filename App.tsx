34024d4f5cf4783b88910420288a1a54e0f29dcc
Jay Dasari <djayachandra@outlook.com>
2023-02-01 19:01:34 -0600

    testing

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -7,9 +7,18 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const Tab = createBottomTabNavigator();
 
 const Tab1Screen = () => (
-  <View>
-    <Text>Home</Text>
-  </View>
+  <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={[styles.headerText, styles.headerLeft]}>Left Text</Text>
+        <Text style={[styles.headerText, styles.headerRight]}>Right Text</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.bodyText}>Text 1</Text>
+        <Text style={styles.bodyText}>Text 2</Text>
+        <Text style={styles.bodyText}>Text 3</Text>
+        {/* Add more text components as needed */}
+      </View>
+    </View>
 );
 
 const Tab2Screen = () => (
@@ -82,5 +91,41 @@ const LISDHub = () => (
     </Tab.Navigator>
   </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  headerLeft: {
+    textAlign: "left",
+  },
+  headerRight: {
+    textAlign: "right",
+  },
+  body: {
+    width: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: 20,
+  },
+  bodyText: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+});
 
 export default LISDHub;
