f24a6653d2944682acf70ae2ae5fb9680dcd9e52
CodingPro445 <slowbird1108@gmail.com>
2023-03-29 17:30:03 -0500

    fixed stupid progressbar error

--- App/PeriodTimer.tsx
+++ App/PeriodTimer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ProgressBarAndroid} from 'react-native';
 import * as Progress from 'react-native-progress';
 
 const PeriodTimer = () => {
@@ -10,20 +10,28 @@ const PeriodTimer = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const periods = [
-    { name: '1st Period', startTime: '08:15', endTime: '09:45' },
-    { name: 'Passing Period', startTime: '09:45', endTime: '09:50' },
-    { name: '2nd Period', startTime: '09:50', endTime: '11:20' },
-    { name: 'Ranger Time', startTime: '11:20', endTime: '11:55' },
-    { name: 'Passing Period', startTime: '11:55', endTime: '12:00' },
-    { name: '3rd Period', startTime: '12:00', endTime: '14:00' },
-    { name: 'Passing Period', startTime: '14:00', endTime: '14:05' },
-    { name: '4th Period', startTime: '14:05', endTime: '15:35' },
+  interface Period {
+    name: string;
+    startTime: string;
+    endTime: string;
+  }
+  
+  const periods: Period[] = [
+    { name: 'School Starting in', startTime: '00:00', endTime: '08:15' },
+    { name: '1st Period ends in', startTime: '08:15', endTime: '09:45' },
+    { name: 'Passing Period ends in', startTime: '09:45', endTime: '09:50' },
+    { name: '2nd Period ends in', startTime: '09:50', endTime: '11:20' },
+    { name: 'Ranger Time ends in', startTime: '11:20', endTime: '11:55' },
+    { name: 'Passing Period ends in', startTime: '11:55', endTime: '12:00' },
+    { name: '3rd Period ends in', startTime: '12:00', endTime: '14:00' },
+    { name: 'Passing Period ends in', startTime: '14:00', endTime: '14:05' },
+    { name: '4th Period ends in', startTime: '14:05', endTime: '15:35' },
+    { name: 'Next Day in', startTime: '15:35', endTime: '23:59' },
   ];
 
-  const getRemainingTime = (period: { name?: string; startTime?: string; endTime: any; }) => {
+  const getRemainingTime = (period: Period) => {
     const now = currentTime.getTime();
-    const [hours, minutes] = period.endTime.split(':').map((time: string) => parseInt(time));
+    const [hours, minutes] = period.endTime.split(':').map((time) => parseInt(time));
     const end = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), hours, minutes).getTime();
     const remaining = end - now;
     if (remaining < 0) return '00 hrs 00 mins';
@@ -31,23 +39,8 @@ const PeriodTimer = () => {
     const remainingMinutes = Math.floor((remaining % 3600000) / 60000);
     return `${remainingHours} hrs ${remainingMinutes} mins`;
   };
-  
 
-  const currentPeriod: {
-    name?: string;
-    startTime: string;
-    endTime: string;
-  } = periods.find((period) => {
-    const [startHours, startMinutes] = period.startTime.split(':').map((time) => parseInt(time));
-    const [endHours, endMinutes] = period.endTime.split(':').map((time) => parseInt(time));
-    const start = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), startHours, startMinutes).getTime();
-    const end = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), endHours, endMinutes).getTime();
-    return currentTime.getTime() >= start && currentTime.getTime() < end;
-  }) || { name: '', startTime: '', endTime: '' };
-  
-  
-
-  const getProgress = (period: { startTime: string; endTime: string }) => {
+  const getProgress = (period: Period) => {
     if (!currentPeriod) {
       return 0; // return default value of 0 when currentPeriod is undefined
     }
@@ -60,41 +53,34 @@ const PeriodTimer = () => {
     const duration = end - start;
     return elapsed / duration;
   };
-  
 
+  const currentPeriod = periods.find((period) => {
+    const [startHours, startMinutes] = period.startTime.split(':').map((time) => parseInt(time));
+    const [endHours, endMinutes] = period.endTime.split(':').map((time) => parseInt(time));
+    const start = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), startHours, startMinutes).getTime();
+    const end = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), endHours, endMinutes).getTime();
+    return currentTime.getTime() >= start && currentTime.getTime() < end;
+  }) || { name: '', startTime: '', endTime: '' };
+
+  const currentPeriodName = currentPeriod.name || 'No Period';
+  const remainingTime = currentPeriodName === 'No Period' ? 'No School Right Now' : getRemainingTime(currentPeriod);
   return (
-    <View style = {styles.container}>
-      <Text style={styles.period}> {getRemainingTime(currentPeriod)  + " Left in " + currentPeriod?.name ? currentPeriod.name : "No Period" }</Text>
-      {/* <Text>Current Period: {currentPeriod?.name ? currentPeriod.name : "No Period" }{currentPeriod?.name && ( */}
-      <Text style={styles.timer}>{getRemainingTime(currentPeriod)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{currentPeriodName}</Text>
+      <Text style={styles.timer}>{remainingTime}</Text>
       <View style={styles.progressBarContainer}>
-        <Progress.Bar /*styleAttr="Horizontal"*/ indeterminate={false} progress={getProgress(currentPeriod)} color="#0066cc" style={styles.progressBar} />
-      </View>
-      </View>  
-//         <View>
-//           <Text>Time Remaining: {getRemainingTime(currentPeriod)}</Text>
-//           {currentPeriod?.startTime && (
-//   <Progress.Bar 
-//   progress={getProgress(currentPeriod)}
-//   width={null}
-//   color="#6CC644"
-//   unfilledColor="#e6e6e6"
-//   borderRadius={0}
-//   height={20}
-// />
-/* <View style={styles.container}>
-      <Text style={styles.period}>{`${periodSchedule.length === 0 ? '' : 'Left in '}${currentPeriod}th period`}</Text>
-      <Text style={styles.timer}>{timeLeft}</Text>
-      <View style={styles.progressBarContainer}>
-        <ProgressBarAndroid styleAttr="Horizontal" indeterminate={false} progress={progress} color="#0066cc" style={styles.progressBar} />
+        {/* <Progress.Bar
+          // indeterminate={false}
+          progress={getProgress(currentPeriod)}
+          // color="#0066cc"
+          //style={styles.progressBar}
+        /> */}
+        <ProgressBarAndroid styleAttr="Horizontal" indeterminate={false} progress={getProgress(currentPeriod)} color="#0066cc" style={styles.progressBar} />
       </View>
     </View>
-)}
-        </View>
-      )}</Text> */
-    //</View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#ffffff',
@@ -133,9 +119,3 @@ const styles = StyleSheet.create({
 });
 
 export default PeriodTimer;
-
-
-
-
-
-
