4bc7962e23c6d0b464ad33fe91218cc53b99c9df
adithc <adith.chandraiah@gmail.com>
2023-06-13 10:26:05 -0500

    fix?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import { Text } from 'react-native';
 import {AppRegistry} from 'react-native';
 import App from './App/Navigation';
 import {name as appName} from './app.json';
