17c93c7fdae3122438353a6e385ea37bd9d3d051
adithc <adith.chandraiah@gmail.com>
2023-06-22 13:17:21 -0500

    completed grades and attendance

--- App/Grades.tsx
+++ App/Grades.tsx
@@ -1,11 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+interface GradeType {
+  color: string;
+  letter: string;
+}
+
+const getGrade = (score: number): GradeType => {
+  if (score >= 90) return { color: '#00DE64', letter: 'A' };
+  if (score >= 80) return { color: '#3199FE', letter: 'B' };
+  if (score >= 70) return { color: '#F99816', letter: 'C' };
+  return { color: '#FB5B5B', letter: 'D' };
+};
 
 const Grades = () => {
   const [grades, setGrades] = useState<Record<string, string>>({});
-
   const [currentDate, setCurrentDate] = useState('');
 
   useEffect(() => {
@@ -20,19 +31,21 @@ const Grades = () => {
 
   useEffect(() => {
     const dummyData = {
-      Math: "85",
-      English: "92",
-      Science: "78",
-      History: "88",
-      Geography: "90",
-      Art: "95",
-      Music: "80",
-      PE: "88",
+      "AP Computer Science A": "44.25",
+      "AP Physics C: Mechanics": "55.54",
+      "Anatomy and Physiology": "78.99",
+      "AP Human Geography": "88.75",
+      "AP Environmental Science": "90.00",
+      "AP Calculus BC": "100.0",
+      "AP Language and Composition" : "88.46",
+      "AP Spanish IV": "84.34",
     };
 
     setGrades(dummyData);
   }, []);
+  
   const navigation = useNavigation();
+  
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -45,22 +58,34 @@ const Grades = () => {
       ),
     });
   }, [navigation]);
+  
   return (
     <View>
+            <ScrollView>
+
       <View style={styles.header}>
         <Text style={styles.dateText}>{currentDate}</Text>
         <Text style={styles.headerText}>Grades</Text>
       </View>
-      {Object.entries(grades).map(([subject, grade], index) => (
-        <TouchableOpacity style={styles.gradeContainer} key={index}>
-          <View style={styles.gradeItem}>
-            <Text style={styles.gradeText}>{subject}</Text>
-            <View style={styles.gradeBadge}>
-              <Text style={styles.gradeBadgeText}>{grade + "%"}</Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      ))}
+        {Object.entries(grades).map(([subject, grade], index) => {
+          const { color, letter } = getGrade(Number(grade));
+          return (
+            <TouchableOpacity style={styles.gradeContainer} key={index}>
+              <View style={styles.gradeItem}>
+                <View style={styles.gradientTextContainer}>
+                  <Text numberOfLines={1} style={styles.gradeText}>{subject}</Text>
+                </View>
+                <View style={styles.gradeBadge}>
+                  <Text style={styles.gradeBadgeText2}>{letter}</Text>
+                </View>
+                <View style={[styles.gradeBadgeColor, { backgroundColor: color }]}>
+                  <Text style={styles.gradeBadgeText}>{grade }</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
@@ -69,7 +94,7 @@ const styles = StyleSheet.create({
   gradeContainer: {
     backgroundColor: '#E6E6E6',
     borderRadius: 10,
-    padding: 15,
+    padding: 12.5,
     marginVertical: 5,
     marginHorizontal: 10,
   },
@@ -77,20 +102,33 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    marginVertical: -3,
   },
   gradeText: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
   },
   gradeBadge: {
-    backgroundColor: '#CCCCCC',
     borderRadius: 8,
     paddingHorizontal: 11,
     paddingVertical: 5,
+    marginLeft: 10,
+  },
+  gradeBadgeColor: {
+    borderRadius: 8,
+    paddingHorizontal: 11,
+    paddingVertical: 8,
   },
   gradeBadgeText: {
-    color: 'black',
+
+    color: 'white',
     fontSize: 20,
+    fontWeight: 'bold',
+  },
+  gradeBadgeText2: {
+
+    color: '#e8e8e8',
+    fontSize: 0,
     fontWeight: 'bold',
   },
   header: {
@@ -102,19 +140,30 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 40,
     marginLeft: -95,
-    marginBottom:10,
+    marginBottom: 10,
     marginTop: 10,
     color: "#005987",
     fontWeight: "600",
-    
+  },  gradientTextContainer: {
+    flexDirection: 'row',
+    maxWidth: '60%',
+    position: 'relative',
   },
+
+  gradientOverlay: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: '30%',
+    height: '100%',
+  },
+
   dateText: {
     fontSize: 14,
     color: 'gray',
-    marginTop:58,
+    marginTop: 58,
     marginLeft: 14,
   },
 });
-
 
 export default Grades;
