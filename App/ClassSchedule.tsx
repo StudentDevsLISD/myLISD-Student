906128ad89f3b3b5a46b3da11ac816d2bd2a7841
CodingPro445 <slowbird1108@gmail.com>
2023-08-10 00:00:35 -0500

    class schedule

--- App/ClassSchedule.tsx
+++ App/ClassSchedule.tsx
@@ -1,17 +1,64 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { useNavigation } from '@react-navigation/native';
 import { ThemeContext } from './ThemeContext';
 import lightStyles from './LightStyles';
 import darkStyles from './DarkStyles';
+import { ListItem } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { ActivityIndicator } from 'react-native-paper';
+import {IP_ADDRESS} from '@env';
 
-const ClassSchedule: React.FC = () => {
+
+const ClassSchedule = () => {
   const navigation = useNavigation();
 
   const { theme } = useContext(ThemeContext);
   const styles = theme === 'light' ? lightStyles : darkStyles;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [schedule, setSchedule] = useState([]);
 
+  useEffect(() => {
+    loadCredentials();
+  }, []);
+  
+  const loadCredentials = async () => {
+    try {
+      const loadedUsername = await AsyncStorage.getItem('hacusername');
+      const loadedPassword = await AsyncStorage.getItem('hacpassword');
+
+      if (loadedUsername !== null && loadedPassword !== null) {
+        setIsLoggedIn(true);
+        console.log("x")
+        fetchSchedule(loadedUsername, loadedPassword)
+      } else {
+        setIsLoggedIn(false);
+        console.log("y")
+        navigation.navigate("Grades")
+      }
+    } catch (error) {
+      console.log("bad")
+    }
+  };
+  const fetchSchedule = async (username, password) => {
+    let response ='';
+    try {
+      setIsLoading(true);
+      response = await axios.get(`http://${IP_ADDRESS}:8080/schedule?username=${username}&password=${password}`);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsLoggedIn(false);
+      Alert.alert("Error logging in")
+    }      
+    if(response.data){
+      console.log(response.data)
+      setSchedule(response.data.schedule);
+    }
+  };
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -24,9 +71,44 @@ const ClassSchedule: React.FC = () => {
       ),
     });
   }, [navigation]);
+  if (isLoading) {
+    return (
+      <View style={styles.AttendanceLoadingContainer}>
+        <ActivityIndicator
+          animating={true}
+          size={'large'}
+          color={theme === 'light' ? '#005a87' : '#ede1d1'}
+        />
+      </View>
+    );
+  }
   return (
-    <View style={styles.ClassScheduleContainer}>
-      <Text>Jay u suck</Text>
+    <View style={styles.ContactTeacherContainer}>
+    {!isLoggedIn ? (
+      <TouchableOpacity 
+        style={styles.GradesLoginButton} 
+        onPress={() => loadCredentials()}
+      >
+        <Text style={styles.GradesLoginButtonText}>Login with HAC</Text>
+      </TouchableOpacity>
+    ):(
+    <ScrollView contentContainerStyle={styles.HACContainer}>
+    {schedule.map((i) => (
+      <TouchableOpacity key={i.period} onPress={() => {}}>
+        <View style={styles.HACBox}>
+          <ListItem>
+            <Text>{i.period}</Text>
+            <ListItem.Content>
+              <ListItem.Title>{i.description}</ListItem.Title>
+              <ListItem.Subtitle style={styles.HACDescriptionText}>{i.teacher}</ListItem.Subtitle>
+            </ListItem.Content>
+            <Text>{i.room}</Text>
+          </ListItem>
+        </View>
+      </TouchableOpacity>
+    ))}
+    </ScrollView>
+    )}
     </View>
   );
 };
