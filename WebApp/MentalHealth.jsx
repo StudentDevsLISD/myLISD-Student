d357a1acfab5c77ef1eac9a233f48248101a6292
adithc <adith.chandraiah@gmail.com>
2023-09-04 11:37:46 -0500

    mental health

--- WebApp/MentalHealth.jsx
+++ WebApp/MentalHealth.jsx
@@ -1,66 +1,127 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useRef } from "react";
+import { StyleSheet, Text, View, Dimensions, Animated } from "react-native";
 
-class MentalHealthScreen extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* Header */}
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Mental Health</Text>
-        </View>
-
-        {/* Animated Breathing Timer */}
-        <View style={styles.breathingTimer}>
-          {/* Insert your animated breathing timer component here */}
-          {/* You may need a separate component/library for this */}
-        </View>
-
-        {/* Mental Health Resources */}
-        <View style={styles.resources}>
-          {/* Add your mental health resources here */}
-          <TouchableOpacity style={styles.resourceItem}>
-            <Text>Resource 1</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.resourceItem}>
-            <Text>Resource 2</Text>
-          </TouchableOpacity>
-
-          {/* Add more resource items as needed */}
-        </View>
-      </View>
-    );
-  }
+const { width, height } = Dimensions.get("window");
+const circleWidth = width / 2;
+export default function App() {
+  const move = useRef(new Animated.Value(0)).current;
+  const textOpacity = useRef(new Animated.Value(1)).current;
+  Animated.loop(
+    Animated.sequence([
+      Animated.parallel([
+        Animated.timing(textOpacity, {
+          toValue: 1,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+        Animated.timing(move, {
+          toValue: 1,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.parallel([
+        Animated.timing(textOpacity, {
+          delay: 100,
+          toValue: 0,
+          duration: 300,
+          useNativeDriver: true,
+        }),
+        Animated.timing(move, {
+          delay: 1000,
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ])
+  ).start();
+  const translate = move.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, circleWidth / 6],
+  });
+  const exhale = textOpacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          width: circleWidth,
+          height: circleWidth,
+          ...StyleSheet.absoluteFill,
+          alignItems: "center",
+          justifyContent: "center",
+          opacity: textOpacity,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "600",
+          }}
+        >
+          Inhale
+        </Text>
+      </Animated.View>
+      <Animated.View
+        style={{
+          width: circleWidth,
+          height: circleWidth,
+          ...StyleSheet.absoluteFill,
+          alignItems: "center",
+          justifyContent: "center",
+          opacity: exhale,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "600",
+          }}
+        >
+          Exhale
+        </Text>
+      </Animated.View>
+      {[0, 1, 2, 3, 4, 5, 6, 7].map((item) => {
+        const rotation = move.interpolate({
+          inputRange: [0, 1],
+          outputRange: [`${item * 45}deg`, `${item * 45 + 180}deg`],
+        });
+        return (
+          <Animated.View
+            key={item}
+            style={{
+              opacity: 0.1,
+              backgroundColor: "purple",
+              width: circleWidth,
+              height: circleWidth,
+              borderRadius: circleWidth / 2,
+              ...StyleSheet.absoluteFill,
+              transform: [
+                {
+                  rotateZ: rotation,
+                },
+                { translateX: translate },
+                { translateY: translate },
+              ],
+            }}
+          ></Animated.View>
+        );
+      })}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff', // Background color of the screen
-  },
-  header: {
-    padding: 20,
-    backgroundColor: '#007AFF', // Header background color
-    alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 24,
-    color: 'white', // Header text color
-  },
-  breathingTimer: {
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  resources: {
-    marginTop: 20,
-    paddingHorizontal: 20,
-  },
-  resourceItem: {
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-});
-
-export default MentalHealthScreen;
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      left: width / 4,
+      top: height / 4,
+    },
+  });
