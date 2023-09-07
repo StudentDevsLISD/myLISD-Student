0f15b75027886d83b3865a60d9435bcea32a5aff
Jay Dasari <djayachandra@outlook.com>
2023-09-07 04:30:40 -0500

    successful login now goes to the home page

--- WebApp/Login.jsx
+++ WebApp/Login.jsx
@@ -1,16 +1,23 @@
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { GoogleLogin } from '@react-oauth/google';
 import { storeData, retrieveData, removeItem } from './storage.js';
+import { CommonActions, useNavigation } from '@react-navigation/native';
 
 
 const Login = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <GoogleLogin
           onSuccess={credentialResponse => {
             console.log(credentialResponse.credential);
+            navigation.dispatch(
+              CommonActions.reset({
+                routes: [{ name: 'Home' }],
+              })
+            );
           }}
           onError={() => {
             console.log('Login Failed');
